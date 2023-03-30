a282482d91a29387e6efaec5f9bfd9c6174bd069
Thomas Rialland <rialland.thomas@hotmail.fr>
2023-03-30 23:40:22 +0200

    Create Board #show_board, #pretty_board method and tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,13 +7,70 @@ describe Board do
 
   describe '#new_board' do
     it 'returns an array of 7, 6 "empty space" elements array' do
-      expect(board_test.new_board).to eq([[" ", " ", " ", " ", " ", " "],
-                                          [" ", " ", " ", " ", " ", " "],
-                                          [" ", " ", " ", " ", " ", " "],
-                                          [" ", " ", " ", " ", " ", " "],
-                                          [" ", " ", " ", " ", " ", " "],
-                                          [" ", " ", " ", " ", " ", " "],
-                                          [" ", " ", " ", " ", " ", " "]])
+      empty_board = [[" ", " ", " ", " ", " ", " "],
+                     [" ", " ", " ", " ", " ", " "],
+                     [" ", " ", " ", " ", " ", " "],
+                     [" ", " ", " ", " ", " ", " "],
+                     [" ", " ", " ", " ", " ", " "],
+                     [" ", " ", " ", " ", " ", " "],
+                     [" ", " ", " ", " ", " ", " "]]
+      expect(board_test.new_board).to eq(empty_board)
+    end
+  end
+
+  describe '#show_board' do
+
+    it 'calls #pretty_board on @board' do
+      board_attribute = board_test.instance_variable_get(:@board)
+      allow(board_test).to receive(:pretty_board)
+      expect(board_test).to receive(:pretty_board).with(board_attribute)
+      board_test.show_board
+    end
+  end
+
+  describe '#pretty_board' do
+    context 'when board is empty' do
+      it 'should show an empty board' do
+        a = Array.new()
+        7.times {a.push([])}
+        a.each do |element|
+          6.times { element.push(" ") }
+        end 
+        empty_board = <<~BOARD
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                         1   2   3   4   5   6   7
+                      BOARD
+        expect { board_test.pretty_board(a) }.to output(empty_board).to_stdout
+      end
+    end
+    
+    context 'whend board is not empty' do
+
+      it 'should show the state of the board' do
+        a = Array.new()
+        7.times {a.push([])}
+        a.each do |element|
+          6.times { element.push(" ") }
+        end 
+        a[0][0] = 'X'
+        a[3][0] = 'O'
+        a[3][1] = 'X'
+        not_empty_board = <<~BOARD
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [ ] [ ] [ ] [ ]
+                        [ ] [ ] [ ] [X] [ ] [ ] [ ]
+                        [X] [ ] [ ] [O] [ ] [ ] [ ]
+                         1   2   3   4   5   6   7
+                      BOARD
+        expect { board_test.pretty_board(a) }.to output(not_empty_board).to_stdout
+      end
     end
   end
 end
